30bda85e20810ac531a6e2757d4af65f6a438c55
Sufyan Shaikh <absufyn@gmail.com>
2023-03-30 09:15:58 +0000

    adding missed files

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ChatComponent } from './chat/chat.component';
+import { FooterComponent } from './footer/footer.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
